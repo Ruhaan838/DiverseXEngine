f50139a6f75b07183d393c03798f7fd81d8d0676
Ruhaan18 <raiyandalal8@gmail.com>
2025-07-29 22:54:49 +0530

    changes on canvas view

--- Canvas/View/CanvasView.cpp
+++ Canvas/View/CanvasView.cpp
@@ -3,12 +3,19 @@
 //
 
 #include "CanvasView.h"
+
+#include <qevent.h>
+
 #include "../Scene/CanvasScene.h"
 #include <Qt>
 
 CanvasView::CanvasView(CanvasScene *scene_, QWidget *parent)
-    : QGraphicsView(scene_, parent), scene(scene_) {
+    : QGraphicsView(scene_, parent), grScene(scene_) {
     initUI();
+
+    setScene(grScene);
+
+
 }
 
 void CanvasView::initUI() {
@@ -18,4 +25,99 @@ void CanvasView::initUI() {
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
+    setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
+
+}
+
+void CanvasView::mousePressEvent(QMouseEvent *event) {
+    if (event->button() == Qt::MiddleButton) {
+        middleMouseButtonPress(event);
+    } else if (event->button() == Qt::LeftButton) {
+        // printf("Left button pressed\n");
+        leftMouseButtonPress(event);
+    } else if (event->button() == Qt::RightButton) {
+        // printf("Right button pressed\n");
+        rightMouseButtonPress(event);
+    }
+    else {
+        QGraphicsView::mousePressEvent(event);
+    }
+}
+
+void CanvasView::mouseReleaseEvent(QMouseEvent *event) {
+    if (event->button() == Qt::MiddleButton) {
+        middleMouseButtonRelease(event);
+    } else if (event->button() == Qt::LeftButton) {
+        leftMouseButtonRelease(event);
+    } else if (event->button() == Qt::RightButton) {
+        rightMouseButtonRelease(event);
+    } else {
+        QGraphicsView::mouseReleaseEvent(event);
+    }
+}
+
+void CanvasView::middleMouseButtonPress(QMouseEvent *event) {
+
+    setDragMode(QGraphicsView::ScrollHandDrag);
+    auto releaseEvent = QMouseEvent(QEvent::MouseButtonRelease, event->localPos(),
+        event->screenPos(), Qt::LeftButton, Qt::NoButton, event->modifiers());
+    QGraphicsView::mouseReleaseEvent(&releaseEvent);
+    auto fakeEvent = QMouseEvent(event->type(), event->localPos(), event->screenPos(),
+        Qt::LeftButton, event->buttons() | Qt::LeftButton, event->modifiers());
+    QGraphicsView::mousePressEvent(&fakeEvent);
+}
+
+void CanvasView::middleMouseButtonRelease(QMouseEvent *event) {
+    auto fakeEvent = QMouseEvent(event->type(), event->localPos(), event->screenPos(),
+        Qt::LeftButton, event->buttons() & Qt::LeftButton, event->modifiers());
+    QGraphicsView::mouseReleaseEvent(&fakeEvent);
+    setDragMode(QGraphicsView::NoDrag);
+}
+
+// void CanvasView::wheelEvent(QWheelEvent *event) {
+//     // calculate out zoom Factr
+//     float zoomOutFactor = 1 / zoomInFactor;
+//
+//     //calcualte the zoom
+//     float zoomFactor;
+//     if (event->angleDelta().y() > 0) {
+//         zoomFactor = zoomInFactor;
+//         zoom += zoomStep;
+//     }
+//     else {
+//         zoomFactor = zoomOutFactor;
+//         zoom -= zoomStep;
+//     }
+//
+//     //clamping
+//     bool clamped = false;
+//     if (zoom < zoomRange[0]) {
+//         zoom = zoomRange[0];
+//         clamped = true;
+//     } else if (zoom > zoomRange[1]) {
+//         zoom = zoomRange[1];
+//         clamped = true;
+//     }
+//
+//     //set secene scale
+//     if (!clamped | zoomClap == false)
+//         scale(zoomFactor, zoomFactor);
+// }
+
+
+//helper methods
+void CanvasView::leftMouseButtonPress(QMouseEvent *event) {
+    QGraphicsView::mousePressEvent(event);
+}
+
+void CanvasView::leftMouseButtonRelease(QMouseEvent *event) {
+    QGraphicsView::mouseReleaseEvent(event);
+}
+
+void CanvasView::rightMouseButtonPress(QMouseEvent *event) {
+    QGraphicsView::mousePressEvent(event);
+}
+
+void CanvasView::rightMouseButtonRelease(QMouseEvent *event) {
+    QGraphicsView::mouseReleaseEvent(event);
 }
--- Canvas/View/CanvasView.h
+++ Canvas/View/CanvasView.h
@@ -6,15 +6,38 @@
 #define CANVASVIEW_H
 
 #include <QGraphicsView>
+#include <vector>
 
 class CanvasScene;
 
 class CanvasView : public QGraphicsView {
 public:
     explicit CanvasView(CanvasScene *scene_, QWidget *parent = nullptr);
-    CanvasScene *scene;
+    CanvasScene *grScene;
+    float zoomInFactor = 1.25;
+    float zoom = 10;
+    bool zoomClap = true;
+    float zoomStep = 1;
+    std::vector<float> zoomRange = {0, 20};
+
 
     void initUI();
+
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+    // void wheelEvent(QWheelEvent *event) override;
+
+
+    //helper methods
+    void middleMouseButtonPress(QMouseEvent *event);
+    void middleMouseButtonRelease(QMouseEvent *event);
+
+    void leftMouseButtonPress(QMouseEvent *event);
+    void leftMouseButtonRelease(QMouseEvent *event);
+
+    void rightMouseButtonPress(QMouseEvent *event);
+    void rightMouseButtonRelease(QMouseEvent *event);
+
 };
 
 #endif //CANVASVIEW_H
