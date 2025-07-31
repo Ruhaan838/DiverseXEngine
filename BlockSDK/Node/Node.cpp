95b0009fb7deb5bf5721c299aab1ed190266c281
Ruhaan18 <raiyandalal8@gmail.com>
2025-07-31 23:52:14 +0530

    add the sockets

--- BlockSDK/Node/Node.cpp
+++ BlockSDK/Node/Node.cpp
@@ -3,14 +3,15 @@
 //
 
 #include "Node.h"
-#include "Graphics/Graphics.h"
-#include "Widget/Widget.h"
 
 #include "../../Canvas/Scene/CanvasScene.h"
 #include "../Scene/NodeScene.h"
+#include "Graphics/Graphics.h"
+#include "Widget/Widget.h"
+#include "Socket/Socket.h"
 
-Node::Node(Scene *scene_, const std::string &title) : scene(scene_) {
 
+Node::Node(Scene *scene_, const std::string &title, int input_size, int output_size) : scene(scene_) {
     content = new WidgetNode();
 
     grNode = new GraphicsNode(this);
@@ -18,7 +19,32 @@ Node::Node(Scene *scene_, const std::string &title) : scene(scene_) {
     scene->addNode(this);
     scene->grScene->addItem(grNode);
 
+    //create the socket for input
+    int counter = 0;
+    for (int i = 0; i < input_size; i++) {
+        auto *s = new SocketNode(this, counter, POSITION::LEFT_BOTTOM);
+        counter++;
+        inputs.emplace_back(s);
+    }
+    //create the socket for output
+    counter = 0;
+    for (int i = 0; i < output_size; i++) {
+        auto *s = new SocketNode(this, counter, POSITION::RIGHT_BOTTOM);
+        counter++;
+        outputs.emplace_back(s);
+    }
+}
 
+std::pair<int, int> Node::getSocketPos(int index, POSITION position) {
+    int x = 0, y = 0;
 
+    if (position == POSITION::LEFT_TOP || position == POSITION::LEFT_BOTTOM) {x = 0;}
+    else {x = grNode->width;}
+    if (position == POSITION::LEFT_BOTTOM || position == RIGHT_BOTTOM)
+        y = grNode->height - grNode->edge_size - grNode->_padding - index * socket_spacing;
+    else
+        y = grNode->title_height + grNode->_padding + grNode->edge_size + index * socket_spacing;
+
+    return {x, y};
 }
 
--- BlockSDK/Node/Node.h
+++ BlockSDK/Node/Node.h
@@ -4,22 +4,36 @@
 
 #ifndef NODE_H
 #define NODE_H
+
 #include <string>
+#include <vector>
+#include "../Common.h"
 
 class Scene;
 class GraphicsNode;
 class WidgetNode;
+class SocketNode;
+
 
 class Node {
 public:
-    Node(Scene* scene_, const std::string &title = "Undefine Node");
+    Node(Scene* scene_, const std::string &title = "Undefine Node", int input_size=0, int output_size=0);
+
+    std::pair<int, int> getSocketPos(int index, POSITION position);
+
+    std::vector<SocketNode*> inputs;
+    std::vector<SocketNode*> outputs;
+
 
     Scene* scene;
     std::string _title;
 
-    GraphicsNode *grNode{};
-
+    GraphicsNode* grNode;
     WidgetNode *content;
+
+    int socket_spacing = 22;
+
+
 
 };
 
